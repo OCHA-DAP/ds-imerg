05020969c03f5ad35af3424419908f2da770fe83
t-downing <downing.tristan@gmail.com>
2024-06-17 11:00:38 -0700

    add functions

---
+++ src/datasources/imerg.py
@@ -0,0 +1,128 @@
+import datetime
+import os
+import platform
+import shutil
+import tempfile
+from pathlib import Path
+from subprocess import Popen
+from typing import Literal
+
+import pandas as pd
+import requests
+import xarray as xr
+
+from src.utils import blob
+
+IMERG_BASE_URL = (
+    "https://gpm1.gesdisc.eosdis.nasa.gov/data/GPM_L3/GPM_3IMERGD"
+    "{run}.0{version}/{date:%Y}/{date:%m}/3B-DAY-{run}.MS.MRG.3IMERG."
+    "{date:%Y%m%d}-S000000-E235959.V0{version}{version_letter}.nc4"
+)
+
+
+def download_recent_imerg():
+    existing_files = [
+        x.name
+        for x in blob.get_glb_container_client().list_blobs(
+            name_starts_with="imerg/v7"
+        )
+    ]
+    for date in pd.date_range(
+        "2024-06-01", datetime.date.today() - pd.DateOffset(days=1)
+    ):
+        output_blob = f"imerg/v7/{date.strftime('%Y-%m-%d')}.tif"
+        if output_blob in existing_files:
+            print(f"{output_blob} already exists, skipping")
+            continue
+        download_imerg(date)
+        da = process_imerg()
+        upload_imerg(da, output_blob)
+
+
+def download_imerg(
+    date: datetime.datetime,
+    run: Literal["E", "L"] = "L",
+    version: int = 7,
+    save_path: str = Path("temp/imerg_temp.nc"),
+    verbose: bool = False,
+):
+    if os.path.exists(save_path):
+        os.remove(save_path)
+    version_letter = "b" if version == 7 else ""
+    url = IMERG_BASE_URL.format(
+        run=run, date=date, version=version, version_letter=version_letter
+    )
+    if verbose:
+        print("downloading from " + url)
+    result = requests.get(url)
+    try:
+        result.raise_for_status()
+        f = open(save_path, "wb")
+        f.write(result.content)
+        f.close()
+        if verbose:
+            print("contents of URL written to " + save_path)
+    except requests.exceptions.HTTPError as err:
+        print(err)
+        print("failed to download from " + url)
+
+
+def process_imerg(path: str = "temp/imerg_temp.nc"):
+    ds = xr.open_dataset(path)
+    ds = ds.transpose("lat", "lon", "time", "nv")
+    var_name = (
+        "precipitationCal" if "precipitationCal" in ds else "precipitation"
+    )
+    da = ds[var_name]
+    if not ds["time"].dtype == "<M8[ns]":
+        da["time"] = pd.to_datetime(
+            [pd.Timestamp(t.strftime("%Y-%m-%d")) for t in da["time"].values]
+        )
+    da = da.rename({"lon": "x", "lat": "y"}).squeeze(drop=True)
+    return da
+
+
+def upload_imerg(da: xr.DataArray, output_path: str):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".tif") as tmpfile:
+        temp_filename = tmpfile.name
+        da.rio.to_raster(temp_filename, driver="COG")
+        with open(temp_filename, "rb") as f:
+            blob.get_glb_container_client().get_blob_client(
+                output_path
+            ).upload_blob(f, overwrite=True)
+
+
+def create_auth_files():
+    # script to set credentials from
+    # https://disc.gsfc.nasa.gov/information/howto?title=How%20to%20Generate%20Earthdata%20Prerequisite%20Files
+    IMERG_USERNAME = os.environ["IMERG_USERNAME"]
+    IMERG_PASSWORD = os.environ["IMERG_PASSWORD"]
+
+    urs = "urs.earthdata.nasa.gov"  # Earthdata URL to call for authentication
+
+    homeDir = os.path.expanduser("~") + os.sep
+
+    with open(homeDir + ".netrc", "w") as file:
+        file.write(
+            "machine {} login {} password {}".format(
+                urs, IMERG_USERNAME, IMERG_PASSWORD
+            )
+        )
+        file.close()
+    with open(homeDir + ".urs_cookies", "w") as file:
+        file.write("")
+        file.close()
+    with open(homeDir + ".dodsrc", "w") as file:
+        file.write("HTTP.COOKIEJAR={}.urs_cookies\n".format(homeDir))
+        file.write("HTTP.NETRC={}.netrc".format(homeDir))
+        file.close()
+
+    print("Saved .netrc, .urs_cookies, and .dodsrc to:", homeDir)
+
+    # Set appropriate permissions for Linux/macOS
+    if platform.system() != "Windows":
+        Popen("chmod og-rw ~/.netrc", shell=True)
+    else:
+        # Copy dodsrc to working directory in Windows
+        shutil.copy2(homeDir + ".dodsrc", os.getcwd())
+        print("Copied .dodsrc to:", os.getcwd())
